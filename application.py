745919f50849b9888469ef312c2caa96bdbaad15
Albert Nagy <nagy.albert@hotmail.com>
2019-01-17 11:08:00 +0100

    Syntax corrections

--- application.py
+++ application.py
@@ -79,7 +79,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -203,14 +203,14 @@ app.route('/gdisconnect')
 def gdisconnect():
 	access_token = login_session.get('access_token')
 	if access_token:	
-		print 'In gdisconnect access token is %s', access_token
-		print 'User name is: '
-		print login_session['username']
+		# print('In gdisconnect access token is %s', access_token)
+		# print ('User name is: ')
+		# print login_session['username']
 		url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
 		h = httplib2.Http()
 		result = h.request(url, 'GET')[0]
-		print 'result is '
-		print result
+		# print 'result is '
+		# print result
 		if result['status'] == '200':
 			response = make_response(json.dumps('Successfully disconnected.'), 200)
 			response.headers['Content-Type'] = 'application/json'
@@ -219,7 +219,7 @@ def gdisconnect():
 			response = make_response(json.dumps('Failed to revoke token for given user.', 400))
 			response.headers['Content-Type'] = 'application/json'
 			return response
-	print 'Access Token is None'
+	print('Access Token is None')
 	response = make_response(json.dumps('Current user not connected.'), 401)
 	response.headers['Content-Type'] = 'application/json'
 	return response

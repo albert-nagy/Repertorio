2fde0ad5bcf8d8f0036755056b417e688023b8cb
Albert Nagy <nagy.albert@hotmail.com>
2019-01-21 10:40:20 +0100

    Hide My Profile link on my own profile

--- application.py
+++ application.py
@@ -274,7 +274,7 @@ def showProfile(musician_id):
 		c.execute(query, (musician_id,))
 		personal_data = c.fetchone()
 		return render_template('profile.html', personal_data=personal_data,
-			STATE=makeState(), login_session=login_session)
+			url=musician_id, STATE=makeState(), login_session=login_session)
 
 
 

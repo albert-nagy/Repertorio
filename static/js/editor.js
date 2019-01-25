78ed9f00b7e6cd27dcb4f31693a98ed5a0b1d692
Albert Nagy <nagy.albert@hotmail.com>
2019-01-25 10:38:47 +0100

    Function for replacing text via AJAX
    Created a separate function for replacing part of the page to reduce Java and Script file length and keep it simpler.

--- static/js/editor.js
+++ static/js/editor.js
@@ -3,18 +3,7 @@ function getForm(what, id) {
       type: 'POST',
       url: '/infotoedit?what='+what+'&id='+id,
       contentType: 'application/octet-stream; charset=utf-8',
-      success: function(result) {
-         
-          if (what == 'bio')
-                {
-                form = `<form id="editform" action="javascript:void(0)" method="post" onsubmit="editContent(this, 'bio', '`+id+`')">
-                <textarea name="edit_bio">`+result+`</textarea>
-                <button type="submit" class="edit">Submit</button>
-                <button type="reset" class="cancel" onclick="Cancel('bio','`+id+`')">Cancel</button>
-                </form>`;
-                }
-          document.getElementById(what).innerHTML = form;
-      }
+      success: function(result) {replacePart(what,result,id,1);}
       
   }); 
   }
@@ -26,16 +15,7 @@ function editContent(form, what, id) {
       type: 'POST',
       url: '/edit?action=edit&what='+what+'&id='+id+'&text='+encodeURIComponent(content),
       contentType: 'application/octet-stream; charset=utf-8',
-      success: function(result) {
-         
-          if (what == 'bio')
-                {
-                text = `<p>`+result+`</p>
-                <button type="submit" class="edit" onclick="getForm('bio', '`+id+`');">Edit</button>`;
-                }
-          document.getElementById(what).innerHTML = text;
-      }
-      
+      success: function(result) {replacePart(what,result,id,0);}
   }); 
   }
 
@@ -44,14 +24,23 @@ function editContent(form, what, id) {
       type: 'POST',
       url: '/edit?action=cancel&what='+what,
       contentType: 'application/octet-stream; charset=utf-8',
-      success: function(result) {
-          if (what == 'bio')
-                {
-                text = `<p>`+result+`</p>
-                <button type="submit" class="edit" onclick="getForm('bio', '`+id+`');">Edit</button>`;
-                }
-          document.getElementById(what).innerHTML = text;
-      }
-      
+      success: function(result) {replacePart(what,result,id,0);}
   }); 
   }
+
+  function replacePart(what,result,id,form)
+    {
+    if (what == 'bio')
+      {
+      if (form == 0)
+         text = `<p>`+result+`</p>
+          <button type="submit" class="edit" onclick="getForm('bio', '`+id+`');">Edit</button>`;
+      else
+        text = `<form id="editform" action="javascript:void(0)" method="post" onsubmit="editContent(this, 'bio', '`+id+`')">
+                <textarea name="edit_bio">`+result+`</textarea>
+                <button type="submit" class="edit">Submit</button>
+                <button type="reset" class="cancel" onclick="Cancel('bio','`+id+`')">Cancel</button>
+                </form>`;
+      }
+    document.getElementById(what).innerHTML = text;
+    }

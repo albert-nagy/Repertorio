78131fc352f605997abc8585496850cc2b3cbe8b
Albert Nagy <nagy.albert@hotmail.com>
2019-02-11 16:15:31 +0100

    Setup DB filling with demo data from CSV files
    Handle foreign-key constraint problem caused by auto-increment columns

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,5 @@
 import psycopg2
+import csv
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from slugify import slugify
 
@@ -41,6 +42,48 @@ def setupDB(db,c):
 	result = c.fetchone()
 	return result[0]
 
+def fillPresetData(db,c):
+	with open('musicians.csv', 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			c.execute("""INSERT INTO musicians
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+			,row)
+
+	with open('categories.csv', 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			c.execute("INSERT INTO categories (name,creator) VALUES (%s, %s)",
+			(row[1],row[2]))
+
+	with open('works.csv', 'r') as f:
+		reader = csv.reader(f)
+		cat_ids = set()
+		i = 0
+		for row in reader:
+			if row[6] not in cat_ids:
+				i += 1
+				cat_ids.add(row[6])
+			query = """INSERT INTO works (composer,title,
+			duration,instrument,creator,category)
+			VALUES (%s, %s, %s, %s, %s, %s)"""
+			c.execute(query,
+			(row[1],row[2],row[3],row[4],row[5],i))
+	db.commit()
+
+	result = []
+
+	query = "SELECT COUNT(*) FROM musicians"
+	c.execute(query)
+	result.append(c.fetchone()[0])
+	query = "SELECT COUNT(*) FROM categories"
+	c.execute(query)
+	result.append(c.fetchone()[0])
+	query = "SELECT COUNT(*) FROM works"
+	c.execute(query)
+	result.append(c.fetchone()[0])
+	return result
+
 # Check if database exists
 try:
 	db = psycopg2.connect(dbname=dbname) 
@@ -56,6 +99,9 @@ except psycopg2.OperationalError:
 c = db.cursor()
 # Populate new DB and report number of initial inserts
 num = setupDB(db,c)
-print("{} instruments added".format(num))
+
+num_csv = fillPresetData(db,c)
+print("{} instruments, {} musicians, {} categories and {} works added".format(
+num,num_csv[0],num_csv[1],num_csv[2]))
 db.close()
 
